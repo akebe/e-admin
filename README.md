947aab0cbf8bd1bcb973e98dca41733568b90c5a
akebe <akebe@163.com>
2019-11-14 09:43:21 +0800

    publish npm

--- README.md
+++ README.md
@@ -3,7 +3,28 @@
 
 ### Install
 ```
-npm install
+npm install e-admin --save
+```
+
+### Start
+```
+import Vue from 'vue';
+import App from './App.vue';
+
+import ElementUI from 'element-ui';
+import Ea from 'e-admin';
+
+import 'element-ui/lib/theme-chalk/index.css';
+import 'e-admin/lib/index.css';
+
+
+Vue.use(ElementUI).use(Ea);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
 ```
 
 ### LICENSE
--- src/main.js
+++ src/main.js
@@ -3,14 +3,15 @@ import App from './App.vue';
 import router from './router';
 
 import ElementUI from 'element-ui';
-import Ea from '../packages';
+import Ea from 'e-admin';
 
 import 'element-ui/lib/theme-chalk/index.css';
+import 'e-admin/lib/index.css';
+
 import 'prismjs/themes/prism.css';
 import './style/markdown-segmentfault.css';
 
-Vue.use(ElementUI);
-Vue.use(Ea, {});
+Vue.use(ElementUI).use(Ea);
 
 Vue.config.productionTip = false;
 
